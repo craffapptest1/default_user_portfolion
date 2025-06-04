89a7508cc7334befd3de6a0696b6ae14cea8355c
Sayedtaqi313 <141312215+Sayedtaqi313@users.noreply.github.com>
2025-06-04 07:38:50 -0700

    Update start.sh

--- terraform/start.sh
+++ terraform/start.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -e
-
+echo "Current working directory is: $(pwd)"
+pwd
 cd "$(dirname "$0")/.."
-
+echo "Current working directory is: $(pwd)"
+pwd
 # Activate the virtual environment
 source /app/venv/bin/activate
-
+echo "Current working directory is: $(pwd)"
+pwd
 # Upgrade pip and install dependencies
 pip3 install --upgrade pip
 pip3 install fastapi uvicorn
+echo "Current working directory is boefore installtion: $(pwd)"
+pwd
 pip3 install -r ./backend/requirements.txt
+echo "Current working directory is after installion: $(pwd)"
+pwd
 cd app/backend
+echo "Current working directory is beofore running: $(pwd)"
+pwd
 uvicorn main:app --host 0.0.0.0 --port 8080
