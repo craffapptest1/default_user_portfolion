ee24b4446497f691a62cec9dce0d8a95ed862a4d
Sayedtaqi313 <141312215+Sayedtaqi313@users.noreply.github.com>
2025-06-04 06:53:39 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/health")
+async def health():
+    return {"status": "OK"}
 app.include_router(api_router, prefix="/v1")
 
 if __name__ == "__main__":
